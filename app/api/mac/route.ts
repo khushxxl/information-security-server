c6f1398896f415e989b040692acfc1e13caabd10
Khushaal Choithramani <khushaal.choithramani@gmail.com>
2024-10-15 11:24:14 +0100

    api

---
+++ app/api/mac/route.ts
@@ -0,0 +1,21 @@
+// /app/api/mac/route.js
+
+import { NextResponse } from "next/server";
+
+const allowedMacAddrs = ["08:97:98:b2:7e:83"];
+
+export async function POST(request: Request) {
+  try {
+    const body = await request.json();
+    const macAddress = body.mac_address;
+
+    if (allowedMacAddrs.includes(macAddress)) {
+      return Response.json({ approval: true });
+    }
+    return Response.json({ approval: false });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: "Invalid request" }), {
+      status: 400,
+    });
+  }
+}
