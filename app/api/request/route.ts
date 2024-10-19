b62d101e8ae5c197dad8dc89ae89107dab7ab393
Khushaal Choithramani <khushaal.choithramani@gmail.com>
2024-10-19 21:10:16 +0100

    commit changes

--- app/api/request/route.ts
+++ app/api/request/route.ts
@@ -5,15 +5,18 @@ const allowedMacAddrs = ["08:97:98:b2:7e:83"];
 export async function GET(request: Request) {
   try {
     const url = new URL(request.url); // Parse the URL
-    const macAddress: any = url.searchParams.get("mac_address");
+    const macAddress: any = url.searchParams.get("color");
     console.log(url, macAddress);
 
     if (allowedMacAddrs.includes(macAddress)) {
-      return new Response(JSON.stringify({ approval: true }), {
-        status: 200,
-      });
+      return new Response(
+        JSON.stringify({ date: new Date().toTimeString(), mujhe: "ishq" }),
+        {
+          status: 200,
+        }
+      );
     }
-    return new Response(JSON.stringify({ approval: false }), {
+    return new Response(JSON.stringify({ date: new Date().toTimeString() }), {
       status: 200,
     });
   } catch (error) {
